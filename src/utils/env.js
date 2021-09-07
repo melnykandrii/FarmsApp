a26a107e3ba08ac71572cc3486ac63745c22b9e1
Andrii Melnyk <melnik.andrey@gmail.com>
2021-09-07 12:43:15 -0400

    env modification

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 const liveHost = "https://us-central1-farmsnearbyserv.cloudfunctions.net";
 const localHost =
-  "https://079c-24-203-155-215.ngrok.io/farmsnearbyserv/us-central1";
+  "https://eed1-24-203-155-215.ngrok.io/farmsnearbyserv/us-central1";
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const isMock = true;
 export const host = isDevelopment ? localHost : liveHost;
